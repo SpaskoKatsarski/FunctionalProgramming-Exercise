7406e22e36e5827911aa62fa30b83a3c87328af3
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-06-01 00:09:27 +0300

    Updated

---
+++ TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int charSum = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isGreater = (name, sum) => name.ToCharArray().Select(ch => (int)ch).Sum() >= sum;
+
+            Func<string[], int, Func<string, int, bool>, string> firstValidName = (arr, sum, func) => arr.FirstOrDefault(str => func(str, sum));
+
+            string result = firstValidName(names, charSum, isGreater);
+            Console.WriteLine(result);
+        }
+    }
+}
